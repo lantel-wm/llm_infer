730556209541f9089ae08935a89b058b02d1e722
Zhiyu Zhao <zyzh@smail.nju.edu.cn>
2025-03-22 20:04:17 +0800

    [layer] Update mha kernel
    What I did:
    - remove batch_size, query_seq_len, kv_seq_len in constructor
    - add set_batch_size, set_query_seq_len, set_kv_seq_len to
      dynamically change these params before forward call

--- src/layer/mha_layer.cpp
+++ src/layer/mha_layer.cpp
@@ -1,22 +1,22 @@
 #include "mha_layer.hpp"
+#include <cstdint>
 #include "kernel.hpp"
+#include "status.hpp"
 
 namespace layer {
 MultiHeadAttention::MultiHeadAttention(core::DeviceType device_type, int32_t layer_index,
-                                       int32_t num_layers, int32_t batch_size, int32_t kv_mul,
-                                       int32_t kv_size, int32_t query_seq_len, int32_t kv_seq_len,
-                                       int32_t num_heads, int32_t head_size)
+                                       int32_t num_layers, int32_t max_position_embeddings,
+                                       int32_t kv_mul, int32_t kv_size, int32_t num_heads,
+                                       int32_t head_size)
     : Layer(device_type, core::LayerType::MHA, "MultiHead"),
       m_layer_index(layer_index),
       m_num_layers(num_layers),
-      m_batch_size(batch_size),
+      m_max_position_embeddings(max_position_embeddings),
       m_kv_mul(kv_mul),
       m_kv_size(kv_size),
-      m_query_seq_len(query_seq_len),
-      m_kv_seq_len(kv_seq_len),
       m_num_heads(num_heads),
       m_head_size(head_size) {
-  reset_input_size(5);
+  reset_input_size(4);
   reset_output_size(1);
 }
 
@@ -41,22 +41,67 @@ core::Status MultiHeadAttention::forward() {
   return core::error::Success();
 }
 
-// void MultiHeadAttention::set_pos(int32_t pos) { this->m_pos = pos; }
-
 void MultiHeadAttention::set_layer_idx(int32_t layer_idx) { this->m_layer_index = layer_idx; }
+
+void MultiHeadAttention::set_batch_size(int32_t batch_size) { this->m_batch_size = batch_size; }
+
+void MultiHeadAttention::set_query_seq_len(int32_t query_seq_len) {
+  this->m_query_seq_len = query_seq_len;
+}
+
+void MultiHeadAttention::set_kv_seq_len(int32_t kv_seq_len) { this->m_kv_seq_len = kv_seq_len; }
 
 core::Status MultiHeadAttention::check() const {
   core::Status status;
-  const int32_t input_tensor_num = 4;
-  for (int32_t i = 0; i < input_tensor_num; ++i) {
-    // mha score tensor
-    status = check_tensor(get_input(i), m_device_type, m_data_type);
-    if (!status) {
-      LOG(ERROR) << "The input tensor " << std::to_string(i) << " error in the matmul layer.";
-      return status;
-    }
+  const tensor::Tensor& query_tensor = this->get_input(0);
+  const tensor::Tensor& score_tensor = this->get_input(1);
+  const tensor::Tensor& key_cache_tensor = this->get_input(2);
+  const tensor::Tensor& value_cache_tensor = this->get_input(3);
+  const tensor::Tensor& mha_output = this->get_output(0);
+
+  // query_tensor: (batch_size, query_seq_len, num_heads, head_size)
+  status = check_tensor_with_dim(query_tensor, m_device_type, m_data_type, m_batch_size,
+                                 m_query_seq_len, m_num_heads, m_head_size);
+  if (!status) {
+    LOG(ERROR) << "The input tensor 0 (query_tensor) error in the matmul layer.";
+    return status;
   }
-  return check_tensor(get_output(0), m_device_type, m_data_type);
+
+  // score_tensor: (batch_size, num_heads, query_seq_len, kv_seq_len)
+  status = check_tensor_with_dim(score_tensor, m_device_type, m_data_type, m_batch_size,
+                                 m_num_heads, m_query_seq_len, m_kv_seq_len);
+  if (!status) {
+    LOG(ERROR) << "The input tensor 1 (score_tensor) error in the matmul layer.";
+    return status;
+  }
+
+  // key_cache_tensor: (num_layers, batch_size, num_kv_heads, max_position_embeddings, head_size)
+  status = check_tensor_with_dim(key_cache_tensor, m_device_type, m_data_type, m_num_layers,
+                                 m_batch_size, m_num_heads / m_kv_mul, m_max_position_embeddings,
+                                 m_head_size);
+  if (!status) {
+    LOG(ERROR) << "The input tensor 2 (key_cache_tensor) error in the matmul layer.";
+    return status;
+  }
+
+  // value_cache_tensor: (num_layers, batch_size, num_kv_heads, max_position_embeddings, head_size)
+  status = check_tensor_with_dim(value_cache_tensor, m_device_type, m_data_type, m_num_layers,
+                                 m_batch_size, m_num_heads / m_kv_mul, m_max_position_embeddings,
+                                 m_head_size);
+  if (!status) {
+    LOG(ERROR) << "The input tensor 3 (value_cache_tensor) error in the matmul layer.";
+    return status;
+  }
+
+  // mha_output: (batch_size, query_seq_len, num_heads, head_size)
+  status = check_tensor_with_dim(mha_output, m_device_type, m_data_type, m_batch_size,
+                                 m_query_seq_len, m_num_heads, m_head_size);
+  if (!status) {
+    LOG(ERROR) << "The output tensor (mha_output) error in the matmul layer.";
+    return status;
+  }
+
+  return core::error::Success();
 }
 
 }  // namespace layer
--- src/layer/mha_layer.hpp
+++ src/layer/mha_layer.hpp
@@ -8,20 +8,25 @@ namespace layer {
 class MultiHeadAttention : public Layer {
  public:
   explicit MultiHeadAttention(core::DeviceType device_type, int32_t layer_index, int32_t num_layers,
-                              int32_t batch_size, int32_t kv_mul, int32_t kv_size,
-                              int32_t query_seq_len, int32_t kv_seq_len, int32_t num_heads,
-                              int32_t head_size);
+                              int32_t max_position_embeddings, int32_t kv_mul, int32_t kv_size,
+                              int32_t num_heads, int32_t head_size);
 
   core::Status check() const override;
 
-  void set_pos(int32_t pos);
   void set_layer_idx(int32_t layer_idx);
+
+  void set_batch_size(int32_t batch_size);
+
+  void set_query_seq_len(int32_t query_seq_len);
+
+  void set_kv_seq_len(int32_t kv_seq_len);
 
   core::Status forward() override;
 
  private:
   int32_t m_layer_index = 0;
   int32_t m_num_layers = 0;
+  int32_t m_max_position_embeddings = 0;
   int32_t m_batch_size = 0;
   int32_t m_kv_mul = 0;
   int32_t m_kv_size = 0;
